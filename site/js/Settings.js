3f5a31bcd2fdda4185a3e0cd99ee5ae9740e127e
Joey Michael Banks <acktic@outlook.com>
2026-04-03 07:42:01 -0700

    Update Settings.js

--- site/js/Settings.js
+++ site/js/Settings.js
@@ -535,10 +535,10 @@ let exclude = [`Buy`, `Sale`, `Save`, `Price`, `Shop`, `Directions`, `Coupon`, `
 */
 //const cors= `https://oembanks.github.io/corspf/`;
 //const cors = `https://oembanks-1615a0956a22.herokuapp.com/`;
-const cors = `https://cors-anywhere.com/`;
+//const cors = `https://cors-anywhere.com/`;
 //const cors = `https://corsproxy.io/?url=`;
 //const cors = `https://proxy.cors.sh/`;
-//const cors = `https://test.cors.workers.dev/`
+const cors = `https://test.cors.workers.dev/`
 
 /*  translations [array]
 
