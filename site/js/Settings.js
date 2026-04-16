6750c632316fab5a7be12637fa047b6d23404b1d
Joey Michael Banks <acktic@outlook.com>
2026-04-16 00:19:16 -0700

    Update Settings.js

--- site/js/Settings.js
+++ site/js/Settings.js
@@ -533,6 +533,7 @@ let exclude = [`Buy`, `Sale`, `Save`, `Price`, `Shop`, `Directions`, `Coupon`, `
         ty heroku (cors-anywhere instance)
 
 */
+const cors= `https://test.cors.workers.dev`;
 //const cors= `https://oembanks.github.io/corspf/`;
 //const cors = `https://oembanks-1615a0956a22.herokuapp.com/`;
 const cors = `https://api.cors.lol/?url=`;
