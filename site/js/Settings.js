38d1f8455ca7e4e2c110a824f3416240d30b822b
Joey Michael Banks <acktic@outlook.com>
2026-07-09 18:18:54 -0700

    Update CORS configuration in Settings.js
    Uncommented the Heroku CORS URL and commented out the previous CORS proxy URL.

--- site/js/Settings.js
+++ site/js/Settings.js
@@ -537,10 +537,10 @@ let exclude = [`Buy`, `Sale`, `Save`, `Price`, `Shop`, `Directions`, `Coupon`, `
 */
 //const cors= `https://test.cors.workers.dev`;
 //const cors= `https://oembanks.github.io/corspf/`;
-//const cors = `https://oembanks-1615a0956a22.herokuapp.com/`;
+const cors = `https://oembanks-1615a0956a22.herokuapp.com/`;
 //const cors = `https://api.cors.lol/?url=`;
 //const cors = `https://cors-anywhere.com/`;
-const cors = `https://corsproxy.io/?url=`;
+//const cors = `https://corsproxy.io/?url=`;
 //const cors = `https://proxy.cors.sh/`;
 //const cors = `https://whateverorigin.org/get?url=`;
 /*  translations [array]
